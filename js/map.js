431bb6e2a424019acd089313bc53b29dabce5f61
MarinaLik <mylym79@gmail.com>
2018-02-20 19:02:51 +0300

    Добавлено перемещение метки по карте

--- js/map.js
+++ js/map.js
@@ -15,9 +15,8 @@
 
   // определение адреса
   var findAddress = function () {
-    var mainPinCoord = mainPin.getBoundingClientRect();
-    var addressCoordX = Math.floor(mainPinCoord.left + pageXOffset + MAIN_PIN_WIDTH / 2);
-    var addressCoordY = Math.floor(mainPinCoord.top + pageYOffset + MAIN_PIN_HEIGHT);
+    var addressCoordX = Math.floor(window.util.getCoords(mainPin).left + MAIN_PIN_WIDTH / 2);
+    var addressCoordY = Math.floor(window.util.getCoords(mainPin).top + MAIN_PIN_HEIGHT);
     inputAddress.value = addressCoordX + ', ' + addressCoordY;
   };
   findAddress();
@@ -33,22 +32,76 @@
 
   // активация страницы
   var activePage = function () {
-    if (map.classList.contains('map--faded')) {
-      map.classList.remove('map--faded');
-      mapPins.appendChild(renderMap());
-    }
-    if (noticeForm.classList.contains('notice__form--disabled')) {
-      noticeForm.classList.remove('notice__form--disabled');
-    }
+    map.classList.remove('map--faded');
+    mapPins.appendChild(renderMap());
+    noticeForm.classList.remove('notice__form--disabled');
     noticeFieldsets.forEach(function (item) {
-      if (item.hasAttribute('disabled')) {
-        item.removeAttribute('disabled');
-      }
+      item.removeAttribute('disabled');
     });
   };
 
-  mainPin.addEventListener('mouseup', function () {
-    activePage();
+    // границы карты для перемещения метки
+    var borderTop = window.util.getCoords(map).top + 150;
+    var borderBottom = window.util.getCoords(map).top + 500 - MAIN_PIN_HEIGHT/2;
+    var borderLeft = window.util.getCoords(map).left;
+    var borderRight = window.util.getCoords(map).right - MAIN_PIN_WIDTH;
+    console.log(borderTop, borderBottom, borderLeft, borderRight);
+
+  mainPin.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    var mainPinY = mainPin.offsetTop - shift.y;
+    var mainPinX = mainPin.offsetLeft - shift.x;
+    if (mainPinY < borderTop) {
+      mainPin.style.top = borderTop + 'px';
+    } else if (mainPinY > borderBottom) {
+      mainPin.style.top = borderBottom + 'px';
+    } else {
+      mainPin.style.top = mainPinY + 'px';
+    }
+    if (mainPinX < borderLeft) {
+      mainPin.style.left = borderLeft + 'px';
+    } else if (mainPinX > borderRight) {
+      mainPin.style.left = borderRight + 'px';
+    } else {
+      mainPin.style.left = mainPinX + 'px';
+    }
+    findAddress();
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+      if (map.classList.contains('map--faded')) {
+        activePage();
+      }
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
+
+  mainPin.addEventListener('mouseup', function (evt) {
+    evt.preventDefault();
+    if (map.classList.contains('map--faded')) {
+      activePage();
+    }
     findAddress();
   });
 })();
