0e3b5def3b03b8bba4e3e6edfa3b95b90e082971
MarinaLik <mylym79@gmail.com>
2018-02-28 22:34:09 +0300

    Числа вынесены в константы

--- js/backend.js
+++ js/backend.js
@@ -1,11 +1,14 @@
 'use strict';
 
 (function () {
+  var SUCCESS_CODE = 200;
+  var TIMEOUT = 10000;
+
   var xhrTest = function (xhr, onLoad, onError) {
     xhr.responseType = 'json';
 
     xhr.addEventListener('load', function () {
-      if (xhr.status === 200) {
+      if (xhr.status === SUCCESS_CODE) {
         onLoad(xhr.response);
       } else {
         onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
@@ -17,7 +20,7 @@
     xhr.addEventListener('timeout', function () {
       onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
     });
-    xhr.timeout = 10000; // 10s
+    xhr.timeout = TIMEOUT;
   };
 
   window.load = function (onLoad, onError) {
