dcd12a9575f19ab9b0d17f03b757bc858a6e422b
MarinaLik <mylym79@gmail.com>
2018-03-01 22:29:41 +0300

    Удалены неиспользуемые функции

--- js/util.js
+++ js/util.js
@@ -4,22 +4,6 @@ window.util = (function () {
   var DEBOUNCE_INTERVAL = 500;
   var lastTimeout;
   return {
-    randomNumber: function (min, max) {
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-    },
-    arrRandomElem: function (arrName) {
-      return arrName[this.randomNumber(0, arrName.length - 1)];
-    },
-    mixArrRandom: function (arrName) {
-      var mixArr = arrName.slice();
-      for (var i = 0; i < mixArr.length - 1; i++) {
-        var randomIndex = this.randomNumber(0, mixArr.length - 2);
-        var changElem = mixArr[randomIndex];
-        mixArr[randomIndex] = mixArr[mixArr.length - 1];
-        mixArr[mixArr.length - 1] = changElem;
-      }
-      return mixArr;
-    },
     removeChildren: function (elem) {
       while (elem.lastChild) {
         elem.removeChild(elem.lastChild);
@@ -46,5 +30,3 @@ window.util = (function () {
     }
   };
 })();
-// перемешивание элементов в случайном порядке в массиве mixArrRandom()
-// очищение списка removeChildren()
